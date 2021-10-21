23aa908b126db7a2df39f2fc5d568c0bd6f136b1
jirajasensei <jiraja.sensei@gmail.com>
2021-10-21 18:09:10 +0200

    Konstrukcja switch (Clean Code)

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,11 @@ public class Main {
         System.out.print("Wprowadź liczbę, a ja sprawdzę czy jest parzysta: ");
         int userInput=scan.nextInt();
 
-        //tworzę tę zmienną po to, żeby dowiedzieć się czy liczba jest podzielna przez 2 (dzielenie modulo)
-        int oddOrEven = userInput%2;
-
-        //początek konstrukcji "switch". W skrócie, w nawiasie podajemy argument wejściowy do warunku
-        switch(oddOrEven){ //tutaj są dwa "case". "0" i "1". Czyli nasz switch zwróci nam wartość 0 albo 1.
-            case 0: //jeśli switch zwróci 0, to wykona się kod poniżej
+        switch(userInput%2) {
+            case 0:
                 System.out.println(userInput+" to parzysta liczba");
                 break;
-            case 1: //jeśli switch zwróci 1, to wykona się kod poniżej
+            case 1:
                 System.out.println(userInput+" to nieparzysta liczba");
         }
     }
